827b16e815de07ae3c54af962dba4678d4f8a94b
Reevesh Sevar <110165835+ReeveshSevar@users.noreply.github.com>
2024-02-19 14:24:06 +0530

    UserServiceImpl.java

--- User-Service/src/main/java/com/user/UserService/serviceImpl/UserServiceImpl.java
+++ User-Service/src/main/java/com/user/UserService/serviceImpl/UserServiceImpl.java
@@ -8,16 +8,11 @@ import com.user.UserService.model.Ratings;
 import com.user.UserService.model.Users;
 import com.user.UserService.repository.UserRepository;
 import com.user.UserService.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService
